b274524c699eafdbd25c84255a70ebb480fe2c6a
depo <alieldeep111@gmail.com>
2026-06-20 14:23:36 +0300

    creaing repo

--- RAAD/Data/Repositories/WeatherRepository.swift
+++ RAAD/Data/Repositories/WeatherRepository.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+protocol WeatherRepositoryProtocol {
+    
+    func getCurrentWeather(city: String) async throws -> WeatherModel
+    
+    func getForecast(city: String) async throws -> [ForecastModel]
+}
+
+final class WeatherRepository:WeatherRepositoryProtocol {
+    
+    private let remoteDataSource: WeatherRemoteDataSourceProtocol
+    
+    init(remoteDataSource:WeatherRemoteDataSourceProtocol) {
+        self.remoteDataSource = remoteDataSource
+    }
+    
+    func getCurrentWeather(city: String) async throws -> WeatherModel {
+        
+        let dto = try await remoteDataSource.getCurrentWeather(
+            city: city
+        )
+        return WeatherModel(
+            city: dto.location.name,
+            country: dto.location.country,
+            temperature: dto.current.temp_c,
+            condition: dto.current.condition.text,
+            humidity: dto.current.humidity,
+            pressure: dto.current.pressure_mb
+        )
+    }
+    
+    func getForecast(city: String) async throws -> [ForecastModel] {
+        
+        let dto = try await remoteDataSource.getForecast(
+                city: city,
+                days: 3
+            )
+        
+        return dto.forecast.forecastday.map {
+            
+            ForecastModel(
+                date: $0.date,
+                temperature: $0.day.avgtemp_c,
+                condition: $0.day.condition.text
+            )
+        }
+    }
+}
