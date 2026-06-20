8803b64afb018c24310fbe88be3a3dd40d4d7807
depo <alieldeep111@gmail.com>
2026-06-20 14:23:20 +0300

    creating remote data source

--- RAAD/Data/Remote/WeatherRemoteDataSource.swift
+++ RAAD/Data/Remote/WeatherRemoteDataSource.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol WeatherRemoteDataSourceProtocol {
+    
+    func getCurrentWeather( city: String) async throws -> CurrentWeatherResponse
+    
+    func getForecast(city: String, days: Int) async throws -> ForecastResponse
+}
+
+final class WeatherRemoteDataSource: WeatherRemoteDataSourceProtocol {
+    
+    private let networkManager:NetworkManagerProtocol
+    
+    init(networkManager: NetworkManagerProtocol) {
+        self.networkManager = networkManager
+    }
+    
+    func getCurrentWeather(city: String) async throws -> CurrentWeatherResponse {
+        try await networkManager.request(
+            endpoint: .current(city: city)
+        )
+    }
+    
+    func getForecast(city: String, days: Int) async throws -> ForecastResponse {
+        
+        try await networkManager.request(
+            endpoint: .forecast(
+                city: city,
+                days: days
+            )
+        )
+    }
+}
