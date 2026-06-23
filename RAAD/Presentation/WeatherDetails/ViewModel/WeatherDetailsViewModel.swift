30d55a0ee3e267b9b3f99f89f11595b508ebefa2
depo <alieldeep111@gmail.com>
2026-06-24 00:30:37 +0300

    feat(presentation/weather-details): update WeatherDetailsViewModel for selected-hour state
    - Add selectedHour published property to drive hour-by-hour detail display
    - Derive metrics (feels like, humidity, wind, UV) reactively from selectedHour

--- RAAD/Presentation/WeatherDetails/ViewModel/WeatherDetailsViewModel.swift
+++ RAAD/Presentation/WeatherDetails/ViewModel/WeatherDetailsViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+@MainActor
+final class WeatherDetailsViewModel: ObservableObject {
+
+    @Published var hours: [HourlyForecastModel]
+
+    @Published var selectedHour: HourlyForecastModel?
+
+    init(hours: [HourlyForecastModel]) {
+        self.hours = hours
+        self.selectedHour = hours.first
+    }
+
+    func selectHour(_ hour: HourlyForecastModel) {
+        selectedHour = hour
+    }
+}
